ede1d488cfbfd67967fa6b6a70e7ee23ccb49fbe
InkSlob <jessewrigh@yahoo.com>
2016-04-27 11:58:31 -0400

    PySpark EdX

---
+++ ML_lab5.py
@@ -0,0 +1,66 @@
+Interpreting PCA (1a)
+---------------------
+All tests passed
+Sample covariance matrix (1b)
+-----------------------------
+All tests passed
+Covariance function (1c)
+------------------------
+All tests passed
+Eigendecomposition (1d)
+-----------------------
+All tests passed
+PCA Scores (1e)
+---------------
+All tests passed
+PCA Function (2a)
+-----------------
+All tests passed
+PCA on `dataRandom` (2b)
+------------------------
+All tests passed
+3D to 2D (2c)
+-------------
+All tests passed
+Variance explained (2d)
+-----------------------
+All tests passed
+Parse the data (3b)
+-------------------
+All tests passed
+Min and max flouresence (3c)
+----------------------------
+All tests passed
+Fractional signal change (3d)
+-----------------------------
+All tests passed
+PCA on the scaled data (3e)
+---------------------------
+All tests passed
+Aggregation using arrays (4a)
+-----------------------------
+All tests passed
+Recreate with `np.tile` and `np.eye` (4b)
+-----------------------------------------
+All tests passed
+Recreate with `np.kron` (4c)
+----------------------------
+All tests passed
+Aggregate by time (4d)
+----------------------
+All tests passed
+Obtain a compact representation (4e)
+------------------------------------
+All tests passed
+Aggregate by direction (4f)
+---------------------------
+All tests passed
+Compact representation of direction data (4g)
+---------------------------------------------
+All tests passed
+-- 20 cases passed (100.0%) --
+
+
+Your submission token ID is 2442977-f8fc3b358e0a94c6470ad0df15e2d4aa:ip-172-31-22-55
+Please include this submission token ID when you need support for your code submission.
+Your anonymous student ID is 7faf50c0735d4a864e3bead492c3a4d5. Do not post this ID on Piazza.
